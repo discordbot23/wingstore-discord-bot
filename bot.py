6c1f3361778a05106b4f665ae8b244048b9fcf9a
Rey Cardona <sircardona08@gmail.com>
2026-06-08 00:07:09 -0300

    Bot Return Normal

--- bot.py
+++ bot.py
@@ -274,17 +274,10 @@ class SalidaMenu(discord.ui.View):
 async def panel(ctx):
 
     embed = discord.Embed(
-        title=" WingsStore • Registro de Jornada • Human Resources Dept. ",
+        title=" Wings Store • Registro de Jornada • Human Resources Dept. ",
         description="Selecciona una opción",
         color=0x5865F2
     )
-
-    embed = discord.Embed(
-        title=" 🚨 AVISO URGENTE - HUMAN RESOURCES 🚨 ",
-        description=" SE INVITA A TODO EL PERSONAL A LLENAR EL FORMULARIO DE IDENTIFICACIÓN QUE SOLICITA EL DEPARTAMENTO POR MEDIO DE WHATSAPP Y EVITAR UNA NEGATIVA ",
-        color=0x5865F2
-    )
-
 
     embed.add_field(
         name="🟢 Entrada 🟢",
@@ -298,7 +291,7 @@ async def panel(ctx):
         inline=False
     )
 
-    embed.set_footer(text="Sistema de registro automatizado y controlado por HR|Dept.")
+    embed.set_footer(text="Sistema Automatizado y controlado por HR|Dept.")
 
     view = discord.ui.View(timeout=None)
 
